26d5e4eba81f3528322b2d4d85f49f8a923d28b6
Ana Caroline Vasconcellos <124820588+Vasconcellos-cipher@users.noreply.github.com>
2026-06-24 14:27:03 +0000

    projeto final  de estudos de poo

---
+++ projeto_organizacao_de_arquivos/livro.py
@@ -0,0 +1,14 @@
+class Livro:
+    def __init__(self, nome, autor, ano, genero):
+        self.nome = nome
+        self.autor = autor
+        self.ano = ano
+        self.genero = genero
+        self.disponivel = True
+        
+    def mostrar_dados(self):
+        print(f"Nome: {self.nome}")
+        print(f"Autor: {self.autor}")
+        print(f"Ano: {self.ano}")
+        print(f"Gênero: {self.genero}")
+        print(f"Disponível: {self.disponivel}")
---
+++ projeto_organizacao_de_arquivos/main.py
@@ -0,0 +1,10 @@
+from livro import Livro
+
+livro1 = Livro(
+    "Python para Iniciantes",
+    "Ana Souza",
+    2026,
+    "Tecnologia"
+)
+
+livro1.mostrar_dados()
